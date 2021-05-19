bb050d2af21f45c6b1c8b3a86d3f1498ff421b87
Steven Ennis <steven.otieno1@gmail.com>
2021-05-19 10:51:08 +0300

    chore(styled component): implement styled component
    - install styled component
    - add style a title style
    - add wrapper
    
    [Finishes #178209575]

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <Wrapper>
+          <Title>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
         </a>
+          </Title>
+        </Wrapper>
       </header>
     </div>
   );
